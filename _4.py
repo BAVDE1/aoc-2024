d2961793f04541481368997f36c8d058dba85537
Bryce-VE <brycevanewyk4@gmail.com>
2024-12-12 17:55:53 +1100

    4

--- _4.py
+++ _4.py
@@ -1,3 +1,4 @@
+import pprint
 import re
 from copy import deepcopy
 
@@ -20,7 +21,7 @@ def _a():
 
             match_num = 0
             for _ in range(4):
-                if contents[i-match_num][char_i] == ["X", "M", "A", "S"][match_num]:
+                if contents[i - match_num][char_i] == ["X", "M", "A", "S"][match_num]:
                     match_num += 1
                     if match_num == 4:
                         found += 1
@@ -33,14 +34,21 @@ def _a():
         if i < 3:
             return 0
 
+        if reverse:
+            l = ''.join([c_ for c_ in l[::-1]])
+
         found = 0
-        for char_i, char in enumerate(l[3::-1 if reverse else 1]):
+        for char_i, char in enumerate(l[3:]):
             if char != "X":
                 continue
 
             match_num = 0
             for _ in range(4):
-                if contents[i - match_num][char_i+3 - match_num] == ["X", "M", "A", "S"][match_num]:
+                checking_line = contents[i - match_num]
+                if reverse:
+                    checking_line = ''.join([c_ for c_ in checking_line[::-1]])
+
+                if checking_line[char_i + 3 - match_num] == ["X", "M", "A", "S"][match_num]:
                     match_num += 1
                     if match_num == 4:
                         found += 1
@@ -56,8 +64,10 @@ def _a():
         total_found += check_diagonal(line, True)
         i += 1
 
+    contents = [c for c in contents[::-1]]
+
     i = 0
-    for line in contents[::-1]:
+    for line in contents:
         total_found += check_vertical(line)
         total_found += check_diagonal(line)
         total_found += check_diagonal(line, True)
@@ -66,4 +76,30 @@ def _a():
 
 
 def _b():
-    contents = read_file(4)
+    contents = read_file(4).split("\n")
+
+    def check_for_x_mas(l):
+        if i == 0 or i == len(l) - 1:
+            return 0
+
+        found = 0
+        for char_i, char in enumerate(l[1:-1]):
+            if char != "A":
+                continue
+
+            check = [
+                contents[i - 1][char_i], contents[i - 1][char_i + 2],
+                contents[i + 1][char_i], contents[i + 1][char_i + 2],
+            ]
+
+            check_a = ["M", "M", "S", "S"]
+            check_b = ["M", "S", "M", "S"]
+            if check == check_a or check[::-1] == check_a or \
+                    check == check_b or check[::-1] == check_b:
+                found += 1
+        return found
+
+    total_found = 0
+    for i, line in enumerate(contents):
+        total_found += int(check_for_x_mas(line))
+    return total_found
