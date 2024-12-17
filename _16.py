4de58ec1aeb6a6cad598f9a0d2ed93f8c92ce9ba
BAVDE1 <bavde4@gmail.com>
2024-12-17 22:38:43 +1100

    16 :(

--- _16.py
+++ _16.py
@@ -21,79 +21,62 @@ def _a():
     lines = read_file(16).split('\n')
     the_map = [list(line) for line in lines]
 
-    path_positions = []
-    walls = []
-    start_pos = []
-    for y, line in enumerate(lines):
-        for x, char in enumerate(line):
-            p = [x, y]
-            if char == 'S':
-                start_pos = p
-            elif char == 'E':
-                end_pos = p
-            elif char == '.':
-                path_positions.append(p)
-            else:
-                walls.append(p)
-
     def add(position, direction):
         addition = [[0, -1], [0, 1], [-1, 0], [1, 0]][direction]
         return [position[0] + addition[0], position[1] + addition[1]]
 
     # returns list of possible directions perpendicular to given direction
-    def perp_routes(x, y, direction):
+    def perp_routes(pos, direction):
         directions = []
-        to_check = [LEFT, RIGHT] if direction in [UP, DOWN] else [UP, DOWN]
-        for checking_direction in to_check:
-            d_pos = add([x, y], checking_direction)
+        to_check = [RIGHT, LEFT] if direction in [UP, DOWN] else [UP, DOWN]
+        for i, checking_direction in enumerate(to_check):
+            d_pos = add(pos, checking_direction)
             if the_map[d_pos[1]][d_pos[0]] != '#':
                 directions.append(checking_direction)
         return directions
 
+    start_pos = [[x, y] for y, line in enumerate(lines) for x, char in enumerate(line) if char == 'S'][0]
     possible_routes = []
+    lowest_pivot = [140]
+    lowest_score = [10e10]
 
-    def try_route(x, y, move_direction, accumulated_pivots, accumulated_moves, visited):
-        if accumulated_pivots > 1000:  # infinite loop
-            return
-        pos_in_front = [x, y]  # start where you are
-        char_in_front = the_map[y][x]
+    def try_route(position, move_direction, accumulated_pivots, accumulated_moves, visited):
+        the_char = the_map[position[1]][position[0]]
         moving = False
 
-        while char_in_front != '#':
-            if pos_in_front in visited:
-                # the_map[y][x] = '_'
+        while the_char != '#':
+            if position in visited:
                 break
-            visited.append([x, y])
-            # the_map[y][x] = '_'
-            x, y = pos_in_front  # move forward
             accumulated_moves += int(moving)
             moving = True
 
-            # found one route
-            if the_map[y][x] == 'E':
+            # found one route & check if score is less than the current lowest
+            if the_char == 'E':
                 route_stats = [accumulated_pivots, accumulated_moves]
                 if route_stats not in possible_routes:
                     possible_routes.append(route_stats)
-                # the_map[y][x] = 'E'
+
+                    score = (route_stats[0] * 1000) + route_stats[1]
+                    if score < lowest_score[0]:
+                        lowest_pivot[0] = route_stats[0]
+                        lowest_score[0] = score
+                        print(lowest_pivot, lowest_score)
                 break
 
-            the_map[y][x] = str(accumulated_pivots)
-
             # branch off
-            for branch_direction in perp_routes(x, y, move_direction):
-                try_route(*add([x, y], branch_direction), branch_direction, accumulated_pivots + 1, accumulated_moves + 1, deepcopy(visited))
+            for branch_direction in perp_routes(position, move_direction):
+                if accumulated_pivots + 1 > lowest_pivot[0]:  # don't even bother
+                    return
+                visited.append(position)
+                try_route(add(position, branch_direction), branch_direction, accumulated_pivots + 1, accumulated_moves + 1, deepcopy(visited))
 
-            pos_in_front = add([x, y], move_direction)
-            char_in_front = the_map[pos_in_front[1]][pos_in_front[0]]
+            position = add(position, move_direction)
+            the_char = the_map[position[1]][position[0]]
 
-    try_route(start_pos[0], start_pos[1], RIGHT, 0, 0, [])
-
-    lowest = 10e10
-    for route_stats in possible_routes:
-        score = (route_stats[0] * 1000) + route_stats[1]
-        if score < lowest:
-            lowest = score
-    return lowest
+    t = time.time()
+    try_route(start_pos, RIGHT, 0, 0, [])
+    print(time.time() - t)
+    return lowest_score[0]
 
 
 def _b():
